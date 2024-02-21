07a2b1c937d9782645cdd1ee622e46a2c05f9c71
Xulinaria <visahomet@gmail.com>
2024-02-21 18:16:47 +0300

    fronted: add filter btn, fishing

--- frontend/src/pages/fishing.tsx
+++ frontend/src/pages/fishing.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import FilterButton from '@/components/FilterButton'
 
 import { Player } from '@/types/player'
 import BooleanCard from '@/components/cards/BooleanCard'
@@ -18,17 +19,41 @@ export default function Fishing() {
       .then((data) => setPlayer(data))
   }, [])
 
+  const [filter, setFilter] = useState('all')
+
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {Object.values(player.fishes).map((fish) => (
-        <BooleanCard
-          id={fish.id}
-          name={fish.name}
-          quantity={fish.quantity}
-          description={fish.description}
-          key={fish.id}
+    <div className="">
+      <div className="flex gap-2">
+        <FilterButton
+          title="Incomplete"
+          target="incomplete"
+          setFilter={setFilter}
         />
-      ))}
+        <FilterButton
+          title="Complete"
+          target="complete"
+          setFilter={setFilter}
+        />
+      </div>
+      <div className="mt-4 grid grid-cols-4 gap-4">
+        {Object.values(player.fishes)
+          .filter((fish) => {
+            const quantity = fish.quantity
+
+            if (filter === 'incomplete') return quantity === -1
+            else if (filter === 'complete') return quantity >= 1
+            else return true
+          })
+          .map((fish) => (
+            <BooleanCard
+              id={fish.id}
+              name={fish.name}
+              quantity={fish.quantity}
+              description={fish.description}
+              key={fish.id}
+            />
+          ))}
+      </div>
     </div>
   )
 }
